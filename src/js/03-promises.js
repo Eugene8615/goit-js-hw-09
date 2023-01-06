22ce2d251acf926d1197158166b669fc7733bb83
Eugene8615 <reznichenko8615@gmail.com>
2023-01-06 02:46:11 +0200

    2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onCreatePromises);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onCreatePromises(e) {
+  e.preventDefault();
+
+  const amountInputValue = form.elements.amount.value;
+  let delayInputValue = Number(form.elements.delay.value);
+  const stepInputValue = Number(form.elements.step.value);
+
+  for (let i = 1; i <= amountInputValue; i += 1) {
+    createPromise(i, delayInputValue)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delayInputValue += stepInputValue;
   }
 }
